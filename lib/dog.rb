3f8dc33d5ab88b2898b97920290740ee38fca006
Milos Popovic <milos_popovic@outlook.com>
2020-07-22 21:20:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def self.all
+    @all
+  end
+  
+
+end# Add your code here
