101925090238454ee13a1bcf20186656d64bc901
Milos Popovic <milos_popovic@outlook.com>
2020-07-22 21:35:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,10 @@ class Dog
   attr_accessor :name
   def initialize(name)
     @name = name
-    save
+    @@all << name
   end
   def self.save
-    @@all << name
+
   end
   def self.all
     @@all
